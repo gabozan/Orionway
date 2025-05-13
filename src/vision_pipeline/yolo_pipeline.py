164101996f76905bf347ed38de8038356802e946
Gabriel Rios <rioszan@gmail.com>
2025-05-14 01:19:07 +0200

    Commit de pipeline de YOLO. con postprocesado

--- src/vision_pipeline/yolo_pipeline.py
+++ src/vision_pipeline/yolo_pipeline.py
@@ -9,11 +9,13 @@ from ultralytics.engine.results import Results
 from yolo_utils import load_model, predict_image
 from image_utils import load_image, save_image
 from preprocess import letterbox
+from postprocess import calculate_class_center
+from typing import Optional
 from pathlib import Path
 
 # ==================================================================================================================== #
 
-def detect_objects(image_path: Path) -> Results:
+def detect_objects(image_path: Path) -> Optional[int]:
     """
     Detecta objectes en una imatge donada.
 
@@ -21,7 +23,8 @@ def detect_objects(image_path: Path) -> Results:
         image_path (Path): Ruta a la imatge original.
 
     Retorna:
-        Results: Resultats de la predicció del model YOLO.
+        int: Índex de la classe de l’objecte més proper al centre.
+        None: Si no s’ha detectat cap objecte.
     """
     model = load_model(MODEL_OBJECT_DETECTOR)
     img = load_image(image_path)
@@ -29,4 +32,5 @@ def detect_objects(image_path: Path) -> Results:
     processed_img_path = PROCESSED_DIR / f"{image_path.stem}.jpg"
     save_image(processed_img_path, processed_img)
     objects = predict_image(model, processed_img_path)
-    return objects
+    cls = calculate_class_center(objects, processed_img)
+    return cls

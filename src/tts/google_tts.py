4e4a54e8c6e844c63282235e0ae0737ff3b2b7a3
Gabriel Rios <rioszan@gmail.com>
2025-05-15 13:15:45 +0200

    Commit de Text-to-speech a revisar

---
+++ src/tts/google_tts.py
@@ -0,0 +1,33 @@
+########################################################################################################################
+#                                                                                                                      #
+#                                    OrionWay - Robótica, Llenguatge i Planificació                                    #
+#                                                                                                                      #
+########################################################################################################################
+
+from google.cloud import texttospeech
+import os
+
+# ==================================================================================================================== #
+
+def generar_audio(texto, nombre_archivo='audio.mp3'):
+    client = texttospeech.TextToSpeechClient()
+    synthesis_input = texttospeech.SynthesisInput(text=texto)
+    voice = texttospeech.VoiceSelectionParams(
+        language_code="es-ES",
+        ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+    )
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3
+    )
+    response = client.synthesize_speech(
+        input=synthesis_input,
+        voice=voice,
+        audio_config=audio_config
+    )
+    with open(nombre_archivo, "wb") as out:
+        out.write(response.audio_content)
+
+
+clase = "hola"
+texto = f"Tens a la mà l'objecte {clase}"
+generar_audio(texto)

1e044ae938419de2caa7623735095b109d167c59
albert-ce <correuacaes@gmail.com>
2025-05-26 20:37:50 +0200

    IO Arduino Raspy canviat

--- src/main/buttons_control.ino
+++ src/main/buttons_control.ino
@@ -11,7 +11,8 @@ const long debounceDelay = 50;
 const long doubleClickInterval = 300;
 const long holdTimeThreshold = 3000;
 
-typedef enum {
+typedef enum
+{
   BUTTON_NONE = -1,
   BUTTON0_SINGLE_CLICK = 0,
   BUTTON1_SINGLE_CLICK = 1,
@@ -22,51 +23,69 @@ typedef enum {
   BUTTON1_DOUBLE_CLICK = 21
 } ButtonEvent;
 
-void initButtons() {
-  for (int i = 0; i < 3; i++) {
+void initButtons()
+{
+  for (int i = 0; i < 3; i++)
+  {
     pinMode(buttonPins[i], INPUT_PULLUP);
   }
 }
 
-ButtonEvent getButtons(unsigned long currentTime) {
-  for (int i = 0; i < 3; i++) {
+ButtonEvent getButtons(unsigned long currentTime)
+{
+  for (int i = 0; i < 3; i++)
+  {
     estatActual[i] = digitalRead(buttonPins[i]);
 
-    if (estatActual[i] == LOW && estatAnterior[i] == HIGH && (currentTime - lastPressTime[i]) > debounceDelay) {
+    if (estatActual[i] == LOW && estatAnterior[i] == HIGH && (currentTime - lastPressTime[i]) > debounceDelay)
+    {
       lastPressTime[i] = currentTime;
       clickCount[i]++;
       clickState[i] = 1;
       holdStartTime[i] = currentTime;
     }
 
-    if (estatActual[i] == LOW && estatAnterior[i] == LOW) {
-      if (clickState[i] == 1 && (currentTime - holdStartTime[i]) >= holdTimeThreshold) {
+    if (estatActual[i] == LOW && estatAnterior[i] == LOW)
+    {
+      if (clickState[i] == 1 && (currentTime - holdStartTime[i]) >= holdTimeThreshold)
+      {
         clickState[i] = 3;
         return (ButtonEvent)(i + 10);
       }
     }
 
-    if (estatActual[i] == HIGH && estatAnterior[i] == LOW) {
+    if (estatActual[i] == HIGH && estatAnterior[i] == LOW)
+    {
       lastReleaseTime[i] = currentTime;
-      if (clickState[i] != 3) {
+      if (clickState[i] != 3)
+      {
         clickState[i] = 1;
-      } else {
+      }
+      else
+      {
         clickState[i] = 0;
       }
     }
 
-    if (clickState[i] != 3 && (currentTime - lastReleaseTime[i]) > doubleClickInterval && lastReleaseTime[i] > 0) {
-      if (clickCount[i] == 1) {
+    if (clickState[i] != 3 && (currentTime - lastReleaseTime[i]) > doubleClickInterval && lastReleaseTime[i] > 0)
+    {
+      if (clickCount[i] == 1)
+      {
         clickCount[i] = 0;
         clickState[i] = 0;
         lastReleaseTime[i] = 0;
         return (ButtonEvent)i;
-      } else if (clickCount[i] == 2) {
+      }
+      else if (clickCount[i] == 2)
+      {
         clickCount[i] = 0;
         clickState[i] = 0;
         lastReleaseTime[i] = 0;
-        if (i == 1) return BUTTON1_DOUBLE_CLICK;
-      } else {
+        if (i == 1)
+          return BUTTON1_DOUBLE_CLICK;
+      }
+      else
+      {
         clickCount[i] = 0;
         clickState[i] = 0;
         lastReleaseTime[i] = 0;
--- src/main/raspberry.ino
+++ src/main/raspberry.ino
@@ -1,17 +1,44 @@
-void initRaspberryIO(){
-    Serial.begin(115200);
+void initRaspberryIO()
+{
+  Serial.begin(115200);
 }
 
-int readFromRaspberry()
+void sendInstructionToRaspberry(uint8_t code)
 {
-  if (Serial.available())
+  Serial.write((uint8_t)0x01); // HEADER: Instrucció
+  Serial.write(code);          // 1 byte
+}
+
+void sendDataToRaspberry(float val)
+{
+  Serial.write((uint8_t)0x02);                // HEADER: Dada float
+  Serial.write((uint8_t *)&val, sizeof(val)); // 4 bytes
+}
+
+int readInstructionFromRaspberry()
+{
+  if (Serial.available() >= 2)
   {
-    return Serial.read() - '0';
+    uint8_t header = Serial.read();
+    if (header == 0x01 && Serial.available() >= 1)
+    {
+      return Serial.read();
+    }
   }
   return -1;
 }
 
-void sendToRaspberry(int i)
+float readFloatFromRaspberry()
 {
-  Serial.println(i);
+  if (Serial.available() >= 5)
+  {
+    uint8_t header = Serial.read();
+    if (header == 0x02 && Serial.available() >= 4)
+    {
+      float val;
+      Serial.readBytes((byte *)&val, sizeof(val));
+      return val;
+    }
+  }
+  return NAN;
 }

562db7378b9874bf2d69fd426d01cce97b1142ae
LevonKG <levonkesoyan@gmail.com>
2025-05-27 21:41:22 +0200

    first approach aproximament

--- src/main/main.ino
+++ src/main/main.ino
@@ -14,7 +14,24 @@ RobotState estat = ATURAT;
 bool objectesDetectats[3];
 ButtonEvent button = BUTTON_NONE;
 RobotState giraSegüent = ATURAT;
-float angle=0;
+float angle = 0;
+
+// APROPAMENT: variables i subestats
+enum ApropSubState : uint8_t {
+  SEARCH_PERSON,
+  ORIENT_ROBOT,
+  FORWARD_TO_OBSTACLE,
+  CHECK_ARRIVAL,
+  AVOID_OBSTACLE
+};
+ApropSubState subState = SEARCH_PERSON;         // subestat
+int  scanAngle = angleStartFromFOV();           // angle actual de barrida
+unsigned long lastScanTs = 0;                   // temporitzador de camera
+int  foundAngle = 0;                            // angle relatiu a la persona
+uint16_t bbWidth = 0;                           // bounding‑box
+int  avoidTurnDeg = 0;                          // +90 / –90  (esquerra / dreta)
+
+
 
 void setup()
 {
@@ -27,83 +44,169 @@ void setup()
 void loop()
 {
   unsigned long currentTime = millis();
-  switch (estat){
+  switch (estat) {
 
     //=====================//
     //       ATURAT        //
     //=====================//
-    case ATURAT:
-      //--------------[ ACCIONS ]--------------
-      stopMotors();
+  case ATURAT:
+    //--------------[ ACCIONS ]--------------
+    stopMotors();
 
-      //-----------[ CANVIS D'ESTAT ]----------
+    //-----------[ CANVIS D'ESTAT ]----------
 
-      // Dos cops de mans
-      if(getTaps())
-      {
-        estat = APROPAMENT;
-        break;
-      }
-
-      // Pulsació al botó central (una o dues vegades)
-      button = getButtons(currentTime);
-      switch (button){
-        case BUTTON1_SINGLE_CLICK:
-          estat = AVANÇA;
-          break;
-        case BUTTON1_DOUBLE_CLICK:
-          estat = RECONEIX;
-          sendToRaspberry(RECONEIX);
-          break;
-      }
-
+    // Dos cops de mans
+    if (getTaps())
+    {
+      estat = APROPAMENT;
       break;
+    }
+
+    // Pulsació al botó central (una o dues vegades)
+    button = getButtons(currentTime);
+    switch (button) {
+    case BUTTON1_SINGLE_CLICK:
+      estat = AVANÇA;
+      break;
+    case BUTTON1_DOUBLE_CLICK:
+      estat = RECONEIX;
+      sendToRaspberry(RECONEIX);
+      break;
+    }
+
+    break;
 
     //=====================//
     //      RECONEIX       //
     //=====================//
-    case RECONEIX:
+  case RECONEIX:
 
-      //-----------[ CANVIS D'ESTAT ]----------
+    //-----------[ CANVIS D'ESTAT ]----------
 
-      // La Raspberry retorna el mateix codi = Ha acabat correctament
-      if(readFromRaspberry() == RECONEIX)
-      {
-        estat = ATURAT;
-        break;
-      }
+    // La Raspberry retorna el mateix codi = Ha acabat correctament
+    if (readFromRaspberry() == RECONEIX)
+    {
+      estat = ATURAT;
       break;
+    }
+    break;
 
     //=====================//
     //      PETICIÓ       //
     //=====================//
-    case PETICIÓ:
-      RevisaObstacles(objectesDetectats);  // Es pot treure si veiem que a AVANÇA ja es fa i no fa falta repetir-ho
+  case PETICIÓ:
+    RevisaObstacles(objectesDetectats);  // Es pot treure si veiem que a AVANÇA ja es fa i no fa falta repetir-ho
 
-      //-----------[ CANVIS D'ESTAT ]----------
+    //-----------[ CANVIS D'ESTAT ]----------
 
-      // Si no es detecten obstacles en la direcció de la petició
-      if((!objectesDetectats[0] && button == BUTTON0_LONG_PRESS) || (!objectesDetectats[2] && button == BUTTON2_LONG_PRESS))
-      {
-        angle = button == BUTTON0_LONG_PRESS? -90.0 : 90.0;
-        estat = GIRA;
-        giraSegüent = AVANÇA;
-        break;
-      }
-      else
-      {
-        estat = AVANÇA;
-        break;
-      }
+    // Si no es detecten obstacles en la direcció de la petició
+    if ((!objectesDetectats[0] && button == BUTTON0_LONG_PRESS) || (!objectesDetectats[2] && button == BUTTON2_LONG_PRESS))
+    {
+      angle = button == BUTTON0_LONG_PRESS ? -90.0 : 90.0;
+      estat = GIRA;
+      giraSegüent = AVANÇA;
       break;
+    }
+    else
+    {
+      estat = AVANÇA;
+      break;
+    }
+    break;
 
-    
+
     //=====================//
     //        GIRA         //
     //=====================//
-    case GIRA:
-      rotate(angle);
-      estat = giraSegüent;
+  case GIRA:
+    rotate(angle);
+    estat = giraSegüent;
+    break;
+
+
+
+    //=====================//
+    //     APROPAMENT      //
+    //=====================//
+  case APROPAMENT:
+  {
+    switch (subState) {
+
+    case SEARCH_PERSON:
+      if (hasElapsed(lastScanTs)) {
+        rotateCamera(scanAngle);
+        if (detectPerson(foundAngle, bbWidth)) {
+          rotateCameraToCenter();
+          subState = ORIENT_ROBOT;
+        }
+        else {
+          scanAngle = nextScanAngle(scanAngle);
+          if (hasCompletedScan(scanAngle)) {
+            //No es troba ningú --> torna a ATURAT
+            estat = ATURAT;
+          }
+        }
+        lastScanTs = millis();
+      }
       break;
+
+    case ORIENT_ROBOT:
+      rotate(foundAngle);
+      rotateCameraToCenter();
+      moveForward();
+      subState = FORWARD_TO_OBSTACLE;
+      break;
+
+    case FORWARD_TO_OBSTACLE:
+      RevisaObstacles(objectesDetectats);
+      if (objectesDetectats[1]) {
+        stopMotors();
+        subState = CHECK_ARRIVAL;
+      }
+      break;
+
+    case CHECK_ARRIVAL:
+    {
+      if (detectPerson(foundAngle, bbWidth)) {
+        const uint16_t frameW = getCameraFrameWidth();
+        if (isPersonCloseEnough(bbWidth)) {
+          // Persona trobada
+          estat = ATURAT;
+          subState = SEARCH_PERSON;
+          scanAngle = angleStartFromFOV();
+          rotate(180);
+          break;
+        }
+      }
+      // No es una persona
+      avoidTurnDeg = (objectesDetectats[0] || !objectesDetectats[2]) ? 90 : -90; // preferencia esquerra
+      rotate(avoidTurnDeg);
+      moveForward();
+      subState = AVOID_OBSTACLE;
+      break;
+    }
+
+    case AVOID_OBSTACLE:
+    {
+      bool sideClear = (avoidTurnDeg > 0) ? (objectesDetectats[2])
+        : (objectesDetectats[0]);
+      if (sideClear) {
+        stopMotors();
+        // Tornem a buscar la persona
+        scanAngle = angleStartFromFOV();
+        subState = SEARCH_PERSON;
+      }
+      else if (objectesDetectats[1]) {
+        stopMotors();
+        avoidTurnDeg = (avoidTurnDeg > 0) ? -90 : 90;
+        rotate(avoidTurnDeg);
+        moveForward();
+      }
+      break;
+    }
+    }
+  }
+
+  break;
   }
 }

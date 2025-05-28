b111dd193ffcedcf1e2dc55a1e2ead542b5ab030
Gabriel Rios <rioszan@gmail.com>
2025-05-28 13:03:34 +0200

    commit ultimo modelo

--- cloud-api/app/config.py
+++ cloud-api/app/config.py
@@ -12,7 +12,7 @@ BASE_PATH = Path(__file__).parent.resolve()
 
 # Rutes dels models YOLO
 MODEL_OBJECT_PERSON_DETECTOR = BASE_PATH / "models" / "modelObjectDetector.pt"
-#MODEL_ZEBRAI_DETECTOR = BASE_PATH / "models" / "modelZebraiDetector.pt"
+MODEL_ZEBRAI_DETECTOR = BASE_PATH / "models" / "modelZebrAIDetector.pt"
 
 # Paràmetres de la inferència
 IMAGE_SIZE = 640
